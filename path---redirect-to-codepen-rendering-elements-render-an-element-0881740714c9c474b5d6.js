f3c9776e0b956dde3ebd7006e45662d62503e316
Alexey Pyltsyn <lex61rus@gmail.com>
2018-07-11 03:31:40 +0300

    Updates

---
+++ path---redirect-to-codepen-rendering-elements-render-an-element-0881740714c9c474b5d6.js
@@ -0,0 +1,2 @@
+webpackJsonp([63577948256742],{1018:function(e,t){e.exports={pathContext:{action:"https://codepen.io/pen/define",payload:'{"editors":"0010","html":"<div id=\\"root\\"></div>","js":"const element = <h1>Привет, мир</h1>;\\nReactDOM.render(element, document.getElementById(\'root\'));\\n","js_external":"//unpkg.com/react/umd/react.development.js;//unpkg.com/react-dom/umd/react-dom.development.js","js_pre_processor":"babel","layout":"left"}'}}}});
+//# sourceMappingURL=path---redirect-to-codepen-rendering-elements-render-an-element-0881740714c9c474b5d6.js.map
