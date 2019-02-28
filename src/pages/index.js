dfb39ce4cccc081a14b27bb46c0241b644b26457
Alexey Pyltsyn <lex61rus@gmail.com>
2019-02-28 07:22:45 +0300

    Fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,13 @@ import Flex from 'components/Flex';
 import CodeExample from 'components/CodeExample';
 import PropTypes from 'prop-types';
 import React, {Component} from 'react';
-import {graphql} from 'gatsby';
+import {graphql, Link} from 'gatsby';
 import TitleAndMetaTags from 'components/TitleAndMetaTags';
 import Layout from 'components/Layout';
 import {colors, media, sharedStyles} from 'theme';
 import loadScript from 'utils/loadScript';
 import createOgUrl from 'utils/createOgUrl';
 import {babelURL} from 'site-constants';
-import ReactDOM from 'react-dom';
 import logoWhiteSvg from 'icons/logo-white.svg';
 
 class Home extends Component {
@@ -51,10 +50,29 @@ class Home extends Component {
     return (
       <Layout location={location}>
         <TitleAndMetaTags
-          title="React — JavaScript-библиотека для создания пользовательских интерфейсов"
+          title="React &ndash; JavaScript-библиотека для создания пользовательских интерфейсов"
           ogUrl={createOgUrl('index.html')}
         />
         <div css={{width: '100%'}}>
+                        <div
+        css={{
+          position: 'fixed',
+          left: 0,
+          right: 0,
+          top: 0,
+          padding: '6px 0',
+          height: '36px',
+          textAlign: 'center',
+          background: '#761a85',
+          color: colors.white,
+          zIndex: 6,
+          lineHeight: '1.4',
+        }}
+      >
+      🎉 Читай <span css={{color: colors.brand, fontWeight: 600}}>официальный</span> перевод документации! ➡️ <a href="https://ru.reactjs.org/" css={{textDecoration: 'underline'}}>ru.reactjs.org</a> 🎉
+      (⭐ ...и не забудь поставить «звёздочку» <a href="https://github.com/reactjs/ru.reactjs.org" target="_blank" css={{textDecoration: 'underline'}}>репозиторию</a>! ⭐)
+      </div>
+      
           <header
             css={{
               backgroundColor: colors.dark,
@@ -64,53 +82,31 @@ class Home extends Component {
               css={{
                 paddingTop: 45,
                 paddingBottom: 20,
-                    [media.greaterThan('xlarge')]: {
-                      paddingTop: 20,
-                      fontSize: 30,
-                    },
-                  }}>
-                  JavaScript-библиотека для создания пользовательских интерфейсов
-                </p>
-                <Flex
-                  valign="center"
-                  css={{
-                    paddingTop: 40,
 
-                    [media.greaterThan('xlarge')]: {
-                      paddingTop: 65,
-                    },
-                  }}>
-                  <CtaItem>
-                    <ButtonLink to="/docs/getting-started.html" type="primary">
-                      Начать изучение
-                    </ButtonLink>
-                  </CtaItem>
-                  <CtaItem>
-                    <ButtonLink to="/tutorial/tutorial.html" type="secondary">
-                      Перейти к введению
-                    </ButtonLink>
-                  </CtaItem>
-                </Flex>
-              </Container>
-            </div>
-          </div>
-        </header>
+                [media.greaterThan('small')]: {
+                  paddingTop: 60,
+                  paddingBottom: 70,
+                },
 
-        <Container>
-          <div css={sharedStyles.markdown}>
-            <section
-              css={[
-                sectionStyles,
-                {
-                  [media.lessThan('medium')]: {
-                    marginTop: 0,
-                    marginBottom: 0,
-                    overflowX: 'auto',
-                    paddingTop: 30,
-                    WebkitOverflowScrolling: 'touch',
-                    position: 'relative',
-                    maskImage:
-                      'linear-gradient(to right, transparent, white 10px, white 90%, transparent)',
+                [media.greaterThan('xlarge')]: {
+                  paddingTop: 95,
+                  paddingBottom: 85,
+                  maxWidth: 1500, // Positioning of background logo
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                  position: 'relative',
+                  '::before': {
+                    content: ' ',
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                    backgroundImage: `url(${logoWhiteSvg})`,
+                    backgroundRepeat: 'no-repeat',
+                    backgroundPosition: '100% 100px',
+                    backgroundSize: '50% auto',
+                    opacity: 0.05,
                   },
                 },
               }}>
@@ -156,7 +152,8 @@ class Home extends Component {
                         fontSize: 30,
                       },
                     }}>
-                    A JavaScript library for building user interfaces
+                    JavaScript-библиотека для создания пользовательских
+                    интерфейсов
                   </p>
                   <Flex
                     valign="center"
@@ -171,12 +168,12 @@ class Home extends Component {
                       <ButtonLink
                         to="/docs/getting-started.html"
                         type="primary">
-                        Get Started
+                        Начать работу
                       </ButtonLink>
                     </CtaItem>
                     <CtaItem>
                       <ButtonLink to="/tutorial/tutorial.html" type="secondary">
-                        Take the Tutorial
+                        Перейти к введению
                       </ButtonLink>
                     </CtaItem>
                   </Flex>
@@ -186,18 +183,115 @@ class Home extends Component {
           </header>
 
           <Container>
-            <Flex valign="center">
-              <CtaItem>
-                <ButtonLink to="/docs/getting-started.html" type="primary">
-                  Начать изучение
-                </ButtonLink>
-              </CtaItem>
-              <CtaItem>
-                <ButtonLink to="/tutorial/tutorial.html" type="secondary">
-                  Перейти к введению
-                </ButtonLink>
-              </CtaItem>
-            </Flex>
+            <div css={sharedStyles.markdown}>
+              <section
+                css={[
+                  sectionStyles,
+                  {
+                    [media.lessThan('medium')]: {
+                      marginTop: 0,
+                      marginBottom: 0,
+                      overflowX: 'auto',
+                      paddingTop: 30,
+                      WebkitOverflowScrolling: 'touch',
+                      position: 'relative',
+                      maskImage:
+                        'linear-gradient(to right, transparent, white 10px, white 90%, transparent)',
+                    },
+                  },
+                ]}>
+                <div
+                  css={{
+                    display: 'flex',
+                    flexDirection: 'row',
+
+                    [media.lessThan('medium')]: {
+                      display: 'block',
+                      whiteSpace: 'nowrap',
+                    },
+                  }}>
+                  {marketing.edges.map(({node: column}, index) => (
+                    <div
+                      key={index}
+                      css={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        flex: '0 1 33%',
+                        marginLeft: 40,
+
+                        '&:first-of-type': {
+                          marginLeft: 0,
+
+                          [media.lessThan('medium')]: {
+                            marginLeft: 10,
+                          },
+                        },
+
+                        [media.lessThan('medium')]: {
+                          display: 'inline-block',
+                          verticalAlign: 'top',
+                          marginLeft: 0,
+                          whiteSpace: 'normal',
+                          width: '75%',
+                          marginRight: 20,
+                          paddingBottom: 40,
+
+                          '&:first-of-type': {
+                            marginTop: 0,
+                          },
+                        },
+                      }}>
+                      <h3
+                        css={[
+                          headingStyles,
+                          {
+                            '&&': {
+                              // Make specificity higher than the site-wide h3 styles.
+                              color: colors.subtle,
+                              paddingTop: 0,
+                              fontWeight: 300,
+                              fontSize: 20,
+
+                              [media.greaterThan('xlarge')]: {
+                                fontSize: 24,
+                              },
+                            },
+                          },
+                        ]}>
+                        {column.frontmatter.title}
+                      </h3>
+                      <div dangerouslySetInnerHTML={{__html: column.html}} />
+                    </div>
+                  ))}
+                </div>
+              </section>
+              <hr
+                css={{
+                  height: 1,
+                  marginBottom: -1,
+                  border: 'none',
+                  borderBottom: `1 solid ${colors.divider}`,
+                }}
+              />
+              <section css={sectionStyles}>
+                <div id="examples">
+                  {examples.edges.map(({node}, index) => {
+                    const snippet = code[node.fileAbsolutePath];
+                    return (
+                      <CodeExample
+                        key={index}
+                        id={snippet.id}
+                        code={snippet.code}
+                        containerNodeID={node.frontmatter.domid}
+                        loaded={babelLoaded}>
+                        <h3 css={headingStyles}>{node.frontmatter.title}</h3>
+                        <div dangerouslySetInnerHTML={{__html: node.html}} />
+                      </CodeExample>
+                    );
+                  })}
+                </div>
+              </section>
+            </div>
           </Container>
 
           <section
@@ -211,12 +305,12 @@ class Home extends Component {
               <Flex valign="center">
                 <CtaItem>
                   <ButtonLink to="/docs/getting-started.html" type="primary">
-                    Get Started
+                    Начать работу
                   </ButtonLink>
                 </CtaItem>
                 <CtaItem>
                   <ButtonLink to="/tutorial/tutorial.html" type="secondary">
-                    Take the Tutorial
+                    Перейти к учебнику
                   </ButtonLink>
                 </CtaItem>
               </Flex>
@@ -234,13 +328,6 @@ Home.propTypes = {
     marketing: PropTypes.object.isRequired,
   }).isRequired,
 };
-
-function renderExamplePlaceholder(containerId) {
-  ReactDOM.render(
-    <h4>Загрузка примеров кода...</h4>,
-    document.getElementById(containerId),
-  );
-}
 
 const CtaItem = ({children, primary = false}) => (
   <div
